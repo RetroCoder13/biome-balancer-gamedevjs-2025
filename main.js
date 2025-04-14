b57578d341e84fb39483c29b208996556921d0a1
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2025-04-14 17:37:21 +0100

    Added player walk with animal texture

--- main.js
+++ main.js
@@ -26,6 +26,9 @@ renderManager.addImages("assets/textures/player/idle","player","idle",18)
 renderManager.createState("player","walk",true,4)
 renderManager.addImages("assets/textures/player/walk","player","walk",4)
 
+renderManager.createState("player","walkAnimal",true,4)
+renderManager.addImages("assets/textures/player/walk/animal","player","walkAnimal",4)
+
 animalManager.addAnimal(new Animal(100,100,50,50,1,"rabbit"))
 
 renderManager.createObject("rabbit")
