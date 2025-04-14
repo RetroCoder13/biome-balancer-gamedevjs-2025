79399e02d999f41e1d0275d54b407a7f882081b2
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2025-04-14 22:55:30 +0100

    Updated event handling for animal movement

--- classes/AnimalManager.js
+++ classes/AnimalManager.js
@@ -10,20 +10,19 @@ class AnimalManager{
     update(renderManager,inputManager,player){
         if(inputManager.getKey("e") || inputManager.getKey("r")){
             for(let i=0;i<this.animals.length;i++){
-                if(this.animals[i].x + this.animals[i].w >= player.x
+                if(inputManager.getKey("r") && this.animals[i].held) {
+                    // this.animals[i].y += 50
+                    this.animals[i].targetY = player.y
+                    this.animals[i].held = false
+                    player.holdingAnimal = false
+                } else if(this.animals[i].x + this.animals[i].w >= player.x
                     && this.animals[i].x <= player.x + player.w
                     && this.animals[i].y + this.animals[i].h >= player.y
                     && this.animals[i].y <= player.y + player.h
+                    && inputManager.getKey("e") && !player.holdingAnimal
                 ){
-                    if(inputManager.getKey("e") && !player.holdingAnimal){
-                        this.animals[i].held = true
-                        player.holdingAnimal = true
-                    } else if(inputManager.getKey("r") && this.animals[i].held) {
-                        // this.animals[i].y += 50
-                        this.animals[i].targetY = player.y
-                        this.animals[i].held = false
-                        player.holdingAnimal = false
-                    }
+                    this.animals[i].held = true
+                    player.holdingAnimal = true
                 }
             }
         }
