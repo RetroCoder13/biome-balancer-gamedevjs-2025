b6b102d9c8a93fc7e2c95cb2ffeae8cc9bcd7f38
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2025-04-14 19:22:40 +0100

    You can use e and r to pick up and place animals

--- classes/AnimalManager.js
+++ classes/AnimalManager.js
@@ -7,7 +7,19 @@ class AnimalManager{
         this.animals.push(animal)
     }
 
-    update(renderManager,player){
+    update(renderManager,inputManager,player){
+        if(inputManager.getKey("e") || inputManager.getKey("r")){
+            for(let i=0;i<this.animals.length;i++){
+                if(this.animals[i].x + this.animals[i].w >= player.x
+                    && this.animals[i].x <= player.x + player.w
+                    && this.animals[i].y + this.animals[i].h >= player.y
+                    && this.animals[i].y <= player.y + player.h
+                ){
+                    this.animals[i].held = inputManager.getKey("e")
+                    player.holdingAnimal = inputManager.getKey("e")
+                }
+            }
+        }
         for(let i=0;i<this.animals.length;i++){
             this.animals[i].update(renderManager,player)
         }
