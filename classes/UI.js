798913180adef5bd6debb53f3113e690e288bb0d
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2025-04-14 23:32:24 +0100

    Begun adding UI element

---
+++ classes/UI.js
@@ -0,0 +1,31 @@
+class UI{
+    constructor(){
+        this.active = false
+    }
+
+    setActive(state){
+        this.active = state
+    }
+
+    update(renderManager,inputManager){
+        if(this.active){
+
+        }
+    }
+}
+
+class MapUI extends UI{
+    update(renderManager,inputManager){
+        if(this.active){
+
+        }
+    }
+}
+
+class CompendiumUI extends UI{
+    update(renderManager,inputManager){
+        if(this.active){
+            
+        }
+    }
+}
