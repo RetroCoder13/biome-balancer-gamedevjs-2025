a5104e996d45f6ec6db4a56dcc73361d16ad7d70
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2025-04-23 17:18:54 +0100

    Fixed bug

--- classes/InputManager.js
+++ classes/InputManager.js
@@ -5,19 +5,19 @@ class InputManager{
         this.offsetX = offsetX
         this.offsetY = offsetY
         var that = this
-        addEventListener('mousedown',function(e){
+        window.addEventListener('mousedown',function(e){
             that.mouse.pos = [(e.offsetX/windowScale + cameraManager.x - that.offsetX),(e.offsetY/windowScale + cameraManager.y - that.offsetY)]
             that.mouse.click = true
         })
 
-        addEventListener('mouseup', function(e){
+        window.addEventListener('mouseup', function(e){
             that.mouse.click = false
         })
 
-        addEventListener('keydown',function(e){
+        window.addEventListener('keydown',function(e){
             that.keys[e.key] = true
         })
-        addEventListener('keyup',function(e){
+        window.addEventListener('keyup',function(e){
             that.keys[e.key] = false
         })
     }
